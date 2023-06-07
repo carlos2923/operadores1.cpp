9456b5867dce5a11bb8860456e1ad7e33e00b655
carlos2923 <135257195+carlos2923@users.noreply.github.com>
2023-06-06 19:13:13 -0500

    Create entradascine.cpp

---
+++ entradascine.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char bebida, ubicacion, totalapagar=0;
+    cout << "Ingrese su ubicacion (1-4): ";
+    cin >> ubicacion;
+    cout << "Ingrese cantidad de entradas: ";
+    cin >> cantidad;
+
+    switch (ubicacion) {
+        case 1: "general"
+            total += cantidad * 50;
+            break;
+        case 2: "preferencial"
+            total += cantidad * 80;
+            break;
+        case 3: "platea"
+            total += cantidad * 120;
+            break;
+        case 4: "VIP"
+            total += cantidad * 160;
+            break;
+    }
+
+    cout << "entradas VIP? (1:si, 2:No): ";
+    cin >> opcion;
+    if (opcion == 1) {
+        cout << "Seleccione bebida (G:Gaseosa, C:Cerveza, A:Agua): ";
+        cin >> bebida;
+        switch (bebida) {
+            case 'Gaseosa':
+                total += cantidad * 10;
+                break;
+            case 'Cerveza':
+                total += cantidad * 10;
+                break;
+            case 'Agua':
+                total += cantidad * 10;
+                break;
+        }
+
+        cout << "Total a pagar: $" << total << endl;
+    } else {
+        cout << "Total a pagar: $" << total << endl;
+    }
+
+    return 0;
+}
