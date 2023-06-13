60cd7a46ae664effb448d4ebfac594ac2ba3c8fd
carlos2923 <135257195+carlos2923@users.noreply.github.com>
2023-06-13 08:07:58 -0600

    Update operadores6.cpp
    centimetros a pies

--- operadores6.cpp
+++ operadores6.cpp
@@ -19,3 +19,24 @@ int main() {
 
     return 0;
 } // CONVERTIR CENTIMETROS A PIES
+correccion::
+#include <iostream>
+using namespace std;
+int main() {
+    double pies;
+
+    cout << "Ingrese la medida en centimetros: ";
+    cin >> centimetros;
+
+    double pulgadas =centimetros / 2.54;
+    double piess =pulgadas* 12.0;
+    double yardass = pies * 3.0;
+    double metros = centimetros / 100.0;
+
+    cout << "La medida en yardas es: " << yardas << endl;
+    cout << "La medida en pies es: " << pies << endl;
+    cout << "La medida en pulgadas es: " << pulgadas << endl;
+    cout << "La medida en centímetros es: " << centimetros << endl;
+    cout << "La medida en metros es: " << metros << endl;
+
+    return 0;
